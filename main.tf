939870ceb895ca03bbbe2ad6196282bafe604069
pradeep <p.guta@gmail.com>
2024-04-05 14:09:12 +0530

    vpc terraform

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ resource "aws_ssm_parameter" "params" {
 
 variable "parameters" {
   default = [
-    { name = "prod.rds.master_password", value = "expenseprod1234", type = "SecureString" },
-    { name = "prod.rds.master_username", value = "mysqlrdsadmin", type = "String" },
-    { name = "prod.expense.frontend.backend_url", value = "http://backend-prod.rdevopsb73.online/", type = "String" },
-    { name = "prod.rds.endpoint", value = "prod-mysql.cluster-cbvsbeoyxek4.us-east-1.rds.amazonaws.com", type = "String" },
+    { name = "prod.rds.master_password", value = "Expense12345", type = "SecureString" },
+    { name = "prod.rds.master_username", value = "admin", type = "String" },
+    { name = "prod.expense.frontend.backend_url", value = "http://backend-dev.techadda.co/", type = "String" },
+    { name = "prod.rds.endpoint", value = "prod-mysql.cluster-cdkoq6oem3y2.us-east-1.rds.amazonaws.com", type = "String" },
 ]
     }
